253bb0f2147ed28b59a94123b492863d603753e0
Dipta Mazumder <diptamazumder61@gmail.com>
2021-01-14 09:36:06 +0600

    Practicing type casting

---
+++ Solution 1/project 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi Dipta Mazumder");
+            string s = "Dipta";
+            string s1 = " Mazumder";
+            string s2 = s + s1;
+            Console.WriteLine(s2);
+            int x = 15,y=20;
+            int z = x + y;
+            Console.WriteLine(z);
+            const int a = 45;
+            //int a = 34;
+            Console.WriteLine(a);
+            int D= 5;
+            Console.WriteLine(D);
+            int ASdf = 454;
+            Console.WriteLine(ASdf);
+            double b = 4.55;
+            Console.WriteLine(b);
+
+            long myNum = 15000000000;
+            Console.WriteLine(myNum);
+
+            float myNum1 = 5.75757777777f;//6 digit after decimal point
+            Console.WriteLine(myNum1);
+
+            double myNum2 = 5.75757777777;
+            Console.WriteLine(myNum2);
+
+            float f1 = 35e4f;
+            Console.WriteLine(f1);
+            double f2 = 12E4;
+            Console.WriteLine(f2);
+
+        }
+    }
+}
---
+++ Solution 1/project 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//this all are implicit type casting
+            char ch = 'd';
+            int a = ch;
+            long l = a;
+            float f = l;
+            double d = f;
+            Console.WriteLine(d);
+
+            //these all are explicit type casting example
+            double d1 = 97.775;
+            float f1 =(float) d1;
+            long l1 = (long)f1;
+            Console.WriteLine(l1);
+            int a1 =(int) l1;
+            Console.WriteLine(a1);
+            char ch1 = (char)a1;
+            Console.WriteLine(ch1);
+
+            //But we can also do explicit type casting by using some built in method
+            double d2 = 97.335;
+            float f2 = (float)d2;
+            long l2 = Convert.ToInt64(f2);
+            Console.WriteLine(l2);
+            int a2 = Convert.ToInt32(l2);
+            Console.WriteLine(a2);
+            char ch2 = Convert.ToChar(a2);
+            Console.WriteLine(ch2);
+
+            string s = "13434";
+            long x =Convert.ToInt64(s);
+            Console.WriteLine(x);
+
+        }
+    }
+}
